a9659b0387a6645b63fdd84c820aa1166938b85c
Ksenia Danniker <kseniadanniker@gmail.com>
2019-03-01 00:09:39 +0100

    WIP Homepage

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import { Container } from "reactstrap";
+import homeImage from "../../images/home-image.jpg";
 import UploadWidget from "../UploadWidget";
 
 export default class Home extends Component {
 	render() {
 		return (
 			<Container>
-				<h2>Login</h2>
+				<h2>Welcome</h2>
 				<UploadWidget imageType="profilePic" />
+				<p className="left-container">A website for pet-lovers ...</p>
+				<img className="home-image" src={homeImage} alt="pets"/>
 			</Container>
 		);
 	}
