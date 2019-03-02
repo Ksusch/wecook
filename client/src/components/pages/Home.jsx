8751c01a38da0668cfb20d1b1226f4408cac00d2
Ksenia Danniker <kseniadanniker@gmail.com>
2019-03-02 10:21:41 +0100

    WIP homepage

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,17 +1,28 @@
-import React, { Component } from "react";
-import { Container } from "reactstrap";
-import homeImage from "../../images/home-image.jpg";
-import UploadWidget from "../UploadWidget";
+import React, { Component } from 'react';
+import { Container } from 'reactstrap';
+import UploadWidget from '../UploadWidget';
 
 export default class Home extends Component {
-	render() {
-		return (
-			<Container>
-				<h2>Welcome</h2>
-				<UploadWidget imageType="profilePic" />
-				<p className="left-container">A website for pet-lovers ...</p>
-				<img className="home-image" src={homeImage} alt="pets"/>
-			</Container>
-		);
-	}
+  render() {
+    return (
+      <Container>
+        <h2>Welcome</h2>
+        {/* <UploadWidget imageType="profilePic" /> */}
+        <div className="home-container">
+          <p className="left-container">
+            A website from pet-lovers to pet-lovers.
+            <br />
+            You want to give an opportunity ...
+          </p>
+          <div className="circular">
+            <img
+              className="home-image"
+              src="../../images/home-image.jpg"
+              alt="pets"
+            />
+          </div>
+        </div>
+      </Container>
+    );
+  }
 }
