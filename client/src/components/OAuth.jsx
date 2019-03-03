957fa187d3c6c59fef7b1f601440e441e2cda10c
Ksenia Danniker <kseniadanniker@gmail.com>
2019-03-03 17:02:46 +0100

    Aaron: Fixed merge conflicts

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,167 +1,146 @@
 // code in this file is based on an excellent medium article
 // link: https://codeburst.io/react-authentication-with-twitter-google-facebook-and-github-862d59583105
 
-import React, { Component } from "react";
-import io from "socket.io-client";
-import ServerUrl from "../api/api";
-import { Button } from "reactstrap";
-const providers = ["google", "facebook", "twitter", "instagram"];
+import React, { Component } from 'react';
+import io from 'socket.io-client';
+import ServerUrl from '../api/api';
+import { Button } from 'reactstrap';
+const providers = ['google', 'facebook', 'twitter', 'instagram'];
 // socket.on('connect', () => {console.log(socket.id)});
 
 // console.log(socket)
 class OAuthWrapper extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			socket: false	
-		}
-		this.socket = io(ServerUrl)
-		this.checkSocket = this.checkSocket.bind(this)
-	}
-	
-	componentDidMount() {
-		this.checkSocket()
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      socket: false
+    };
+    this.socket = io(ServerUrl);
+    this.checkSocket = this.checkSocket.bind(this);
+  }
 
-	checkSocket() {
-		let socketCheck;
-		if (this.socket.id === undefined) {	
-			// this.socket.on('reconnect_attempt', () => {console.log("reconnect_attempt: ", this.socket)})
-			// this.socket.on('connect', () => {console.log("connect: ")})
-			// this.socket.on('error', (error) => {console.log("error :", error)})
-			// this.socket.on('disconnect', () => {console.log("disconnect :")})
-			// this.socket.on('data', () => {console.log("data :")})
-			// this.socket.on('message', () => {console.log("message :")})
-			socketCheck = setInterval(() => {
-				if (this.socket.id !== undefined) {
-					this.setState({
-						socket: true
-					})
-					clearInterval(socketCheck);
-				}
-			}, 500);
-		}
-	}
+  componentDidMount() {
+    this.checkSocket();
+  }
 
-	render() {
-		return (
-			this.state.socket ? 
-			<div className="d-flex">
-				{providers.map(v => (
-					<OAuth
-						provider={v}
-						key={v}
-						socket={this.socket}
-						handler={user => this.props.handler(user)}
-					/>
-				))}
-			</div>
-			:
-			<div/>
-		);
-	}
+  checkSocket() {
+    let socketCheck;
+    if (this.socket.id === undefined) {
+      // this.socket.on('reconnect_attempt', () => {console.log("reconnect_attempt: ", this.socket)})
+      // this.socket.on('connect', () => {console.log("connect: ")})
+      // this.socket.on('error', (error) => {console.log("error :", error)})
+      // this.socket.on('disconnect', () => {console.log("disconnect :")})
+      // this.socket.on('data', () => {console.log("data :")})
+      // this.socket.on('message', () => {console.log("message :")})
+      socketCheck = setInterval(() => {
+        if (this.socket.id !== undefined) {
+          this.setState({
+            socket: true
+          });
+          clearInterval(socketCheck);
+        }
+      }, 500);
+    }
+  }
+
+  render() {
+    return this.state.socket ? (
+      <div className="d-flex">
+        {providers.map(v => (
+          <OAuth
+            provider={v}
+            key={v}
+            socket={this.socket}
+            handler={user => this.props.handler(user)}
+          />
+        ))}
+      </div>
+    ) : (
+      <div />
+    );
+  }
 }
 
 class OAuth extends Component {
-	state = {
-		disabled: "",
-	};
+  state = {
+    disabled: ''
+  };
 
-	componentDidMount() {
-		this.props.socket.on(this.props.provider, user => {
-			this.popup.close();
-			this.props.handler(user);
-		});
-	}
+  componentDidMount() {
+    this.props.socket.on(this.props.provider, user => {
+      this.popup.close();
+      this.props.handler(user);
+    });
+  }
 
-	// Routinely checks the popup to re-enable the login button
-	// if the user closes the popup without authenticating.
-	checkPopup() {
-		const check = setInterval(() => {
-			const { popup } = this;
-			if (!popup || popup.closed || popup.closed === undefined) {
-				clearInterval(check);
-				this.setState({ disabled: "" });
-			}
-		}, 1000);
-	}
+  // Routinely checks the popup to re-enable the login button
+  // if the user closes the popup without authenticating.
+  checkPopup() {
+    const check = setInterval(() => {
+      const { popup } = this;
+      if (!popup || popup.closed || popup.closed === undefined) {
+        clearInterval(check);
+        this.setState({ disabled: '' });
+      }
+    }, 1000);
+  }
 
-	// Launches the popup by making a request to the server and then
-	// passes along the socket id so it can be used to send back user
-	// data to the appropriate socket on the connected client.
-	openPopup() {
-		const width = 600,
-			height = 600,
-			left = window.innerWidth / 2 - width / 2,
-			top = window.innerHeight / 2 - height / 2,
-			url = `${ServerUrl}/auth/${this.props.provider}?socketId=${
-				this.props.socket.id
-				}`;
+  // Launches the popup by making a request to the server and then
+  // passes along the socket id so it can be used to send back user
+  // data to the appropriate socket on the connected client.
+  openPopup() {
+    const width = 600,
+      height = 600,
+      left = window.innerWidth / 2 - width / 2,
+      top = window.innerHeight / 2 - height / 2,
+      url = `${ServerUrl}/auth/${this.props.provider}?socketId=${
+        this.props.socket.id
+      }`;
 
-		return window.open(
-			url,
-			"",
-			`toolbar=no, location=no, directories=no, status=no, menubar=no, 
+    return window.open(
+      url,
+      '',
+      `toolbar=no, location=no, directories=no, status=no, menubar=no, 
             scrollbars=no, resizable=no, copyhistory=no, width=${width}, 
             height=${height}, top=${top}, left=${left}`
-		);
-	}
+    );
+  }
 
-	// Kicks off the processes of opening the popup on the server and listening
-	// to the popup. It also disables the login button so the user can not
-	// attempt to login to the provider twice.
-	startAuth(e) {
-		if (!this.state.disabled) {
-			e.preventDefault();
-			this.popup = this.openPopup();
-			this.checkPopup();
-			this.setState({ disabled: "disabled" });
-		}
-	}
+  // Kicks off the processes of opening the popup on the server and listening
+  // to the popup. It also disables the login button so the user can not
+  // attempt to login to the provider twice.
+  startAuth(e) {
+    if (!this.state.disabled) {
+      e.preventDefault();
+      this.popup = this.openPopup();
+      this.checkPopup();
+      this.setState({ disabled: 'disabled' });
+    }
+  }
 
-	closeCard() {
-		this.setState({ user: {} });
-	}
+  closeCard() {
+    this.setState({ user: {} });
+  }
 
-	render() {
-		let iconClass;
-		if (this.props.provider === "google") {
-			iconClass = "fab fa-google fa-5x";
-		} else if (this.props.provider === "twitter") {
-			iconClass = "fab fa-twitter fa-5x";
-		} else if (this.props.provider === "facebook") {
-			iconClass = "fab fa-facebook-f fa-5x";
-		}	else {
-			iconClass = "fab fa-instagram fa-5x";
-		}
-		return (
-<<<<<<< HEAD
-			<div className="oauth-container">
-<<<<<<< HEAD
-				<Button onClick={this.startAuth.bind(this)} className="btn-info">
-					<span className="provider-name">Continue with {providerName}</span>
-					<FontAwesome name={this.props.provider} />
-				</Button>
-=======
-				<div className={"button-wrapper fadein-fast"}>
-					<button
-						onClick={this.startAuth.bind(this)}
-						className={`${iconClass} provider-button`}
-					/>
-				</div>
->>>>>>> c3c492e25e0dd116f76f36935f44f2be5ea5d416
-=======
-			<div className="btn-wrapper">
-				<Button
-					onClick={this.startAuth.bind(this)}
-					className="connection-icon"
-				>
-				<i className={`${iconClass}`}/>
-				</Button>
->>>>>>> 22da2517bd3a00b0d23283fdf12e0cd94a4f3892
-			</div>
-		);
-	}
+  render() {
+    let iconClass;
+    if (this.props.provider === 'google') {
+      iconClass = 'fab fa-google fa-5x';
+    } else if (this.props.provider === 'twitter') {
+      iconClass = 'fab fa-twitter fa-5x';
+    } else if (this.props.provider === 'facebook') {
+      iconClass = 'fab fa-facebook-f fa-5x';
+    } else {
+      iconClass = 'fab fa-instagram fa-5x';
+    }
+    return (
+      <div className="btn-wrapper">
+        <Button onClick={this.startAuth.bind(this)} className="connection-icon">
+          <i className={`${iconClass}`} />
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default OAuthWrapper;
--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,30 +1,29 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
 import { BrowserRouter, Link } from 'react-router-dom';
-import Login from "../pages/Login";
-import Signup from '../pages/Signup'
+import Login from '../Login';
+import Signup from '../Signup';
 import UploadWidget from '../UploadWidget';
 
 export default class Home extends Component {
   render() {
     return (
       <Container>
-				<div className="header">
-					<h2>Welcome to wePet</h2>
-					<img src="../../images/pawprint.png" alt="paws"/>
-				</div>
+        <div className="header">
+          <h2>Welcome to wePet</h2>
+          <img src="../../images/pawprint.png" alt="paws" />
+        </div>
         {/* <UploadWidget imageType="profilePic" /> */}
         <div className="home-container">
-					<div className="left-container">
-						<p>
-            A website from pet-lovers to pet-lovers.
-						</p>
-          	<p>
-            Join the community and look for pets in your surrounding to visit, to sit or become a host for your pet. 
-						</p>
-						<Link to="/signup">Signup</Link> or 
-						<Link to="/login"> Login</Link>
-					</div>
+          <div className="left-container">
+            <p>A website from pet-lovers to pet-lovers.</p>
+            <p>
+              Join the community and look for pets in your surrounding to visit,
+              to sit or become a host for your pet.
+            </p>
+            <Link to="/signup">Signup</Link> or
+            <Link to="/login"> Login</Link>
+          </div>
           <div className="circular">
             <img
               className="home-image"
