a4c35afca638a78710c608c6ee42f85ef1354d89
Goldziher <nhirschfeld@gmail.com>
2019-03-04 13:09:39 +0100

    navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,40 +1,34 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand, Nav, NavItem } from "reactstrap";
 import { NavLink } from "react-router-dom";
 import "../styles.scss";
 
 export default class NavBar extends Component {
 	render() {
 		return (
-			<div>
-				<Navbar color="light" light expand="md">
-					<NavbarBrand href="/">WEPET</NavbarBrand>
-					{/* <NavbarToggler onClick={this.toggle} />
-					<Collapse isOpen={this.state.isOpen} navbar> */}
-					<Nav className="ml-auto" navbar>
-						<NavItem>
-							<NavLink to="/">Find my pet a company</NavLink>
-						</NavItem>
-						<NavItem>
-							<NavLink to="/">Look for pets</NavLink>
-						</NavItem>
-						{this.props.user ? (
-							<NavItem>
-								<NavLink to="/profile">Profile</NavLink>
-							</NavItem>
-						) : (
-								<div />
-							)}
-						{this.props.user ? (
-							<NavItem>
-								<NavLink to="/logout">Logout</NavLink>
-							</NavItem>
-						) : (
-								<div />
-							)}
-					</Nav>
-				</Navbar>
-			</div>
+			<nav className="d-flex justify-content-between">
+				<NavLink to="/" exact className="navbar-link align-self-start">
+					<i className="fas fa-paw" />
+				</NavLink>
+				<div className="align-self-end d-flex justify-content-between">
+					<NavLink to="/" exact className="navbar-link">
+						<i className="fas fa-search" />
+					</NavLink>
+					{this.props.user ? (
+						<NavLink to="/profile" exact className="navbar-link">
+							<i className="fas fa-user" />
+						</NavLink>
+					) : (
+						<div />
+					)}
+					{this.props.user ? (
+						<NavLink to="/logout" exact className="navbar-link">
+							<i className="fas fa-sign-out-alt" />
+						</NavLink>
+					) : (
+						<div />
+					)}
+				</div>
+			</nav>
 		);
 	}
 }
