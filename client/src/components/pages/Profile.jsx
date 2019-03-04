3205f57f8df9c4d9046af4c160b17f3cb5590a80
Ruby Camille <Ruby@Rubys-MacBook-Air.local>
2019-03-04 15:55:03 +0100

    changing profile

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import UploadWidget from "../UploadWidget";
-import { ApiService } from "../../api/api"
+import { ApiService } from "../../api/api";
+import { Container } from 'reactstrap';
 import Img from 'react-image'
 // import '../styles.scss';
 
@@ -21,42 +22,44 @@ class Profile extends Component {
   handleImage(url) {
     console.log("url received", url)
     this.ApiService.updateUser({
-      image: url}).then(user => this.props.handler(user))
+      image: url
+    }).then(user => this.props.handler(user))
   }
   handleSubmit(e) {
     e.preventDefault()
     if (this.state.name !== null || this.state.about !== null) {
       this.ApiService.updateUser(this.state)
-      .then(user => this.props.handler(user))
+        .then(user => this.props.handler(user))
     }
   }
   render() {
     console.log(this.props.user)
-		return (
-			<div>
-      <div>
-          picture: 
-          <Img src={this.props.user.image} alt="profile"/>
+    return (
+      <div className="container">
+        halllllooooo
+        <div className="row justify-content-md-center">
+          picture:
+          <Img src={this.props.user.image} alt="profile" />
         </div>
-      <div>
+        <div>
           Name: {this.props.user.name}
         </div>
         <div>
           About: {this.props.user.about}
         </div>
         <UploadWidget
-							imageType="profilePic"
-							handler={url => this.handleImage(url)}
-							class="upload-widget"
-						/>
+          imageType="profilePic"
+          handler={url => this.handleImage(url)}
+          class="upload-widget"
+        />
         <form onSubmit={(e) => this.handleSubmit(e)}>
-            Name: <input type="text" name="name" onChange={(e) => this.handleChange(e)}/>
-            About: <input type="textarea" name="about" onChange={(e) => this.handleChange(e)}/>
-            <button type="submit">Submit me!</button>
+          Name: <input type="text" name="name" onChange={(e) => this.handleChange(e)} />
+          About: <input type="textarea" name="about" onChange={(e) => this.handleChange(e)} />
+          <button type="submit">Submit me!</button>
         </form>
       </div>
-		);
-	}
+    );
+  }
 }
 
 export default Profile
