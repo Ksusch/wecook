a2d327bf1df63da86f39780adbf844bfc755107e
Goldziher <nhirschfeld@gmail.com>
2019-03-07 17:35:44 +0100

    fix

--- server/bin/server.js
+++ server/bin/server.js
@@ -7,7 +7,7 @@ const socketServer = require('socket.io');
 const io = new socketServer(server).origins('http://localhost:*');
 app.set('io', io);
 app.use((req, res, next) => {
-	res.sendFile(__dirname + '/public/index.html');
+	res.sendFile(__dirname + '../public/index.html');
 });
 require('../src/routing')(app);
 io.on('connection', () => console.log('socket connected'));
